0d8de581d0275c2a3beb2fb8fe0c1737a4859509
Henrry <152703384+lwqk2iorjk@users.noreply.github.com>
2025-04-08 20:47:29 +0800

    Create ex4-4.py

---
+++ ex4-4.py
@@ -0,0 +1,3 @@
+a = int(input("Enter your number 1: "))
+b = int(input("Enter your number 2: "))
+print("a + b =", a + b,", a - b =", a - b,", a × b =", a * b,", a ÷ b =",  a / b,", aᵇ =",  a ** b,", Remainder of a ÷ b =",  a % b)
