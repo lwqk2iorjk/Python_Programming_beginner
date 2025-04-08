13f32d723b8379f15cfd020bc367cf07e6362b2f
Henrry <152703384+lwqk2iorjk@users.noreply.github.com>
2025-04-08 19:25:06 +0800

    Update ex4-1.py

--- ex4-1.py
+++ ex4-1.py
@@ -1,3 +1,7 @@
 name = input("Enter your name: ")
+age = int(input("Enter your AGE: "))
+height = float(input("Enter your height(m): "))
 print(type(name))
-print("Hello, " + name)
+print(type(age))
+print(type(height))
+print("You are", age, "years old", "Your height is ", height, name)
