dc757917ed2d82004c8e4893643da8a56812ece7
Henrry <152703384+lwqk2iorjk@users.noreply.github.com>
2025-04-08 20:49:19 +0800

    Update ex4-4.py

--- ex4-4.py
+++ ex4-4.py
@@ -1,3 +1,3 @@
-a = int(input("Enter your number 1: "))
-b = int(input("Enter your number 2: "))
+a = int(input("Enter your number a: "))
+b = int(input("Enter your number b: "))
 print("a + b =", a + b,", a - b =", a - b,", a × b =", a * b,", a ÷ b =",  a / b,", aᵇ =",  a ** b,", Remainder of a ÷ b =",  a % b)
