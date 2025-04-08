809b10236490961aebbc24d4dc4cacc9c6aceb2c
Henrry <152703384+lwqk2iorjk@users.noreply.github.com>
2025-04-08 19:51:38 +0800

    Update ex4-2.py

--- ex4-2.py
+++ ex4-2.py
@@ -1,4 +1,4 @@
 print("I will do the math for you")
 a = int(input("Enter your number 1: "))
 b = int(input("Enter your number 2: "))
-print(a + b, a - b, a * b, a / b)
+print("a + b =", a + b,", a - b =", a - b,", a × b =", a * b,", a ÷ b =",  a / b)
