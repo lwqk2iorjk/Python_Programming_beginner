115f898b0aee1fa26418a82562d9292def1dd88f
Henrry <152703384+lwqk2iorjk@users.noreply.github.com>
2025-04-29 19:24:14 +0800

    Update hw6-1.py

--- hw6-1.py
+++ hw6-1.py
@@ -1,7 +1,9 @@
-year = int(input("請輸入年份："))
+year = int(input("請輸入西元年份："))
 month = int(input("請輸入月份："))
 
-if month == 2:
+if month < 1 or month > 12:
+    print("請輸入1到12之間的月份")
+elif month == 2:
     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
         print("此月份有29天")
     else:
